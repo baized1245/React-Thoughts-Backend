48e4a9cf5b935a68c4eca9b5fca140a7791bba23
unknown <you@example.com>
2023-12-28 17:51:48 +0600

    Backend setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+
+
+// database
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("database is connected successfully");
+   
+   } catch (err) {
+       console.log(err);
+   }
+   
+}
+
+// middlewares
+dotenv.config()
+
+app.listen(process.env.PORT, () => {
+    connectDB();
+    console.log("app running on port process.env.PORT");
+})
+// baized1245
